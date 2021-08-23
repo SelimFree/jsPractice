dcce058cd50c0ffccb500b672bd3bfa85a5d163f
Selim Altayev <selim.altayev@gmail.com>
2021-08-23 15:50:45 +0500

    practice 4, code refactor

--- main.js
+++ main.js
@@ -1,74 +1,84 @@
-function start() {
-    numberOfFilms = parseInt(prompt("How many you have watched?", ""));
-
-    while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
-        numberOfFilms = parseInt(prompt("How many you have watched?", ""));
-    }
-
-    personalMovieDB.count = numberOfFilms;
-}
-
-
-function getMyAnswers() {
-    for (let i = 0; i < 2; i++) {
-        const movie = prompt("Last movie watched", "");
-        const rating = prompt("What rating will you give", "");
-        if (movie != "" && rating != "" && movie != null && rating != null && movie.length < 50 && rating.length < 50) {
-            personalMovieDB.movies[movie] = rating;
-        }
-        else {
-            i--;
-        }
-    }
-}
-
-
-function checkMyLevel() {
-    if (personalMovieDB.count < 10) {
-        console.log("You have watched few films");
-    } else if (personalMovieDB.count < 30) {
-        console.log("You are a classic viewer");
-    } else if (personalMovieDB.count > 30) {
-        console.log("You are movie-monster!");
-    } else {
-        console.log("Error!");
-    }
-}
-
-function showMyDB() {
-    if (personalMovieDB.privat) {
-        console.log(personalMovieDB);
-    }
-}
-
-
-function writeMyGenres() {
-    for (let i = 0; i < 3; i++) {
-        const myGenre = prompt(`What is you favourite genre №${i + 1}`);
-        if (myGenre != "" && myGenre != null && myGenre.length < 50) {
-            personalMovieDB.genres[i] = myGenre; 
-        }
-        else {
-            i--;
-        }
-    }
-}
-
-
-let numberOfFilms;
+'use strict';
 const personalMovieDB  = {
     count: 0,
     movies: {},
     actors: {},
     genres: [],
-    privat: true
+    privat: true,
+
+    start: function() {
+        let numberOfFilms = parseInt(prompt("How many you have watched?", ""));
+
+        while (numberOfFilms == "" || numberOfFilms == null || isNaN(numberOfFilms)) {
+            numberOfFilms = parseInt(prompt("How many you have watched?", ""));
+        }
+    
+        this.count = numberOfFilms;
+    },
+
+    getAnswers: function() {
+        for (let i = 0; i < 2; i++) {
+            const movie = prompt("Last movie watched", "");
+            const rating = prompt("What rating will you give", "");
+            if (movie != "" && rating != "" && movie != null && rating != null && movie.length < 50 && rating.length < 50) {
+                this.movies[movie] = rating;
+            }
+            else {
+                i--;
+            }
+        }
+    },
+
+    checkLevel: function() {
+        console.log(this.count);
+        if (this.count < 10) {
+            console.log("You have watched few films");
+        } else if (this.count < 30) {
+            console.log("You are a classic viewer");
+        } else if (this.count > 30) {
+            console.log("You are movie-monster!");
+        } else {
+            console.log("Error!");
+        }
+    },
+
+    showDB: function() {
+        if (this.privat) {
+            console.log(this);
+        }
+    },
+
+    writeGenres: function() {
+        for (let i = 0; i < 3; i++) {
+            const myGenre = prompt(`What is you favourite genre №${i + 1}`);
+            if (myGenre != "" && myGenre != null && myGenre.length < 50) {
+                this.genres[i] = myGenre; 
+            }
+            else {
+                i--;
+            }
+        }
+        let j = 1;
+        this.genres.forEach(genre => {
+           console.log(`Your favourite genre №${j} is ${genre}`);
+           j++;
+        });
+    },
+
+    triggerVisible: function() {
+        this.privat = !(this.privat);
+    }
 };
 
 
-start();
-getMyAnswers();
-checkMyLevel();
-writeMyGenres();
-showMyDB();
+personalMovieDB.start();
+personalMovieDB.getAnswers();
+personalMovieDB.checkLevel();
+personalMovieDB.writeGenres();
+personalMovieDB.showDB();
+
+personalMovieDB.triggerVisible();
+personalMovieDB.showDB();
+
 
 
